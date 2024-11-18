137a3c8dad8801e512c5a89a7fef2880a2a284e7
Manuj Rai <imanujrai7@gmail.com>
2024-11-18 14:07:09 +0530

    modified:   src/app/app.routes.ts 	modified:   src/app/map/map.component.css 	modified:   src/app/map/map.component.html 	modified:   src/app/new-user/new-user.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ChartComponent } from './chart/chart.component';
+import { MapComponent } from './map/map.component';
 
 export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
     { path: 'charts', component: ChartComponent },
+    { path: 'maps', component: MapComponent },
 ];
 
 NgModule({
