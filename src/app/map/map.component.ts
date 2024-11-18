40456e1ddf23cd8ac05e84ef823178f4ecef745c
Manuj Rai <imanujrai7@gmail.com>
2024-11-18 18:14:23 +0530

    Changes to be committed: 	modified:   angular.json 	modified:   src/app/map/map.component.css 	modified:   src/app/map/map.component.html 	modified:   src/app/map/map.component.ts

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,12 +1,31 @@
 
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-map',
   standalone: true,
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent { }
+export class MapComponent { 
+  toggleFullScreen(): void {
+    const mapbox = document.querySelector<HTMLElement>('.mapbox');
+
+    if (mapbox) {
+      // Toggle the "fullscreen" class
+      if (mapbox.classList.contains('fullscreen')) {
+        mapbox.classList.remove('fullscreen');
+      } else {
+        mapbox.classList.add('fullscreen');
+      }
+    } else {
+      console.error("Element with class 'mapbox' not found.");
+    }
+  }
+}
+
+
+
 
 
