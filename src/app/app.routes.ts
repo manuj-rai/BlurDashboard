3ae01822f8605016ab211e9ba1158ebd6a32bfba
Manuj Rai <imanujrai7@gmail.com>
2024-11-21 11:03:46 +0530

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.routes.ts 	new file:   src/app/components/components.component.css 	new file:   src/app/components/components.component.html 	new file:   src/app/components/components.component.spec.ts 	new file:   src/app/components/components.component.ts 	modified:   src/styles.css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ChartComponent } from './chart/chart.component';
 import { MapComponent } from './map/map.component';
+import { ComponentsComponent } from './components/components.component';
 
 export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
+    { path: 'components', component: ComponentsComponent },
     { path: 'charts', component: ChartComponent },
     { path: 'maps', component: MapComponent },
 ];
