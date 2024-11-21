7159083bfafe71799593199f69fb0342348964a0
Manuj Rai <imanujrai7@gmail.com>
2024-11-21 11:27:50 +0530

    modified:   src/app/chart/chart.component.ts 	modified:   src/app/components/components.component.html 	modified:   src/app/sidebar/sidebar.component.css 	modified:   src/app/weather/weather.component.css

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -6,25 +6,8 @@ import { isPlatformBrowser } from '@angular/common';
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent implements AfterViewInit {
-  @ViewChild('canvas', { static: false }) canvasRef!: ElementRef;
+export class ChartComponent {
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-
-  ngAfterViewInit() {
-    // Only run on the browser (not during SSR)
-    if (isPlatformBrowser(this.platformId)) {
-      const canvas = this.canvasRef.nativeElement;
-      const ctx = canvas.getContext('2d');
-
-      // Clear canvas before drawing
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      // Set fill color and draw a rectangle
-      ctx.fillStyle = 'green';
-      ctx.fillRect(10, 10, 200, 200); // Draw a 200x200 green square
-    }
-  }
 }
 
 
