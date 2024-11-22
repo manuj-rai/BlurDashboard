b4d2a066ed3d83a117e09e31f6ea79fda6edf1c1
Manuj Rai <imanujrai7@gmail.com>
2024-11-22 12:09:46 +0530

    modified:   angular.json 	modified:   package.json 	modified:   src/app/app.config.ts 	modified:   src/app/components/components.component.css 	modified:   src/app/components/components.component.html 	modified:   src/app/components/components.component.ts 	new file:   src/custom-theme.scss 	modified:   src/styles.css

--- src/app/components/components.component.ts
+++ src/app/components/components.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-components',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './components.component.html',
   styleUrl: './components.component.css'
 })
 export class ComponentsComponent {
+  dataSource = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Smith' },
+  ];
+  displayedColumns = ['id', 'name']; // Column definitions
 
 }
